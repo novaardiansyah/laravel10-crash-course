cfa848bb52e6e227733845d7ec474d10f72e78b2
novaardiansyah <novaardiansyah78@gmail.com>
2023-10-03 14:31:17 +0700

    Controller in MVC

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('welcome');
-});
-
-Route::get('/feed', function () {
-  return view('feed');
-});
-
-Route::get('/profile', function () {
-  return view('users.profile');
-});
+Route::get('/', [DashboardController::class, 'index']);
+Route::get('/profile', [DashboardController::class, 'index']);
