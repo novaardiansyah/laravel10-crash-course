e285ad7e7b15a6dd4ec0f34b2f4be1f779388952
novaardiansyah <novaardiansyah78@gmail.com>
2023-10-24 23:26:38 +0700

    Form Submission (create)

--- resources/views/shared/submit-idea.blade.php
+++ resources/views/shared/submit-idea.blade.php
@@ -2,12 +2,16 @@
   Share yours ideas
 </h4>
 <div class="row">
-  <div class="mb-3">
-    <textarea class="form-control" id="idea" rows="3"></textarea>
-  </div>
-  <div class="">
-    <button class="btn btn-dark">
-      Share
-    </button>
-  </div>
+  <form action="{{ route('idea.create') }}" method="post">
+    @csrf
+
+    <div class="mb-3">
+      <textarea class="form-control" id="idea" name="idea" rows="3"></textarea>
+    </div>
+    <div class="">
+      <button type="submit" class="btn btn-dark">
+        Share
+      </button>
+    </div>
+  </form>
 </div>
