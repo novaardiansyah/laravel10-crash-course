0c372f19345e4a379eedabe16ffd793e1db85a1c
novaardiansyah <novaardiansyah78@gmail.com>
2023-11-20 09:54:01 +0700

    Eager Loading (17 queries down to 5)

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -10,6 +10,7 @@ class Idea extends Model
   use HasFactory;
 
   protected $guarded = ['id', 'created_at', 'updated_at'];
+  protected $with = ['user:id,name,email,bio,email', 'comments.user:id,name,email,bio,email'];
 
   public function comments()
   {
